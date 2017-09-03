502c271df90eff65f7e1d8154604102c57162150
rangadon <31005639+rangadon@users.noreply.github.com>
2017-09-03 16:24:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+
+
+//change the text of the main-text
+
+
+
+//Move the Image
+var img=document.getElementById('img');
+img.onclick=function()
+{
+  img.style.marginLeft="100px" ; 
+};
