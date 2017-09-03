df50287bd9a3e02397b56275cf23ffb64d28242b
rangadon <31005639+rangadon@users.noreply.github.com>
2017-09-03 16:33:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,14 @@ console.log('Loaded!');
 
 //Move the Image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function()
 {
-  img.style.marginLeft="100px" ; 
+    var interval=setInterval(moveRight,100);
+   
 };
