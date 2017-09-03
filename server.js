23b55c1b6e88c67a63c0e2ce8dc54dc084ae040e
rangadon <31005639+rangadon@users.noreply.github.com>
2017-09-03 18:26:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ var counter=0;
 app.get('/counter',function(req,res)
 {
    counter=counter+1;
-   res.send(computer.toString());
+   res.send(counter.toString());
 });
 
 app.get('/:articleName',function(req,res){
