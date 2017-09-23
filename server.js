32a2ee6a5d7835ecfe69085e4f1aa570cd86da46
rangadon <31005639+rangadon@users.noreply.github.com>
2017-09-23 11:34:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/', function (req, res) {
      if(err)  {
          res.status(500).send(err,toString());
      }else  {
-         res.send(JSON.stringify(result));
+         res.send(JSON.stringify(result.rows));
      }
   });
   
